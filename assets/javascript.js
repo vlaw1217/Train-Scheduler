551fc33d4659e2ccb256c09169d4efa2aa19057f
Vicky Law <vicky.law1217@gmail.com>
2019-07-17 12:39:39 -0400

    Setup Initialize

---
+++ assets/javascript.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    
+    
+    // Initialize Firebase //
+    var firebaseConfig = {
+        apiKey: "AIzaSyC0duOMYn3EHuQNBy204CxfF7eWE3mVZfA",
+        authDomain: "train-scheduler-1679a.firebaseapp.com",
+        databaseURL: "https://train-scheduler-1679a.firebaseio.com",
+        projectId: "train-scheduler-1679a",
+        storageBucket: "",
+        messagingSenderId: "618216615643",
+        appId: "1:618216615643:web:7423fda1b358945a"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+    
+    
+});
