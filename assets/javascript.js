617e53f934077a64f2e616a4ec6313bf671df3a8
Vicky Law <vicky.law1217@gmail.com>
2019-07-21 14:45:29 -0400

    add delete button

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,41 @@
+// Initialize Firebase //
+
+let firebaseConfig
+    = {
+    apiKey: "AIzaSyC0duOMYn3EHuQNBy204CxfF7eWE3mVZfA",
+    authDomain: "train-scheduler-1679a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1679a.firebaseio.com",
+    projectId: "train-scheduler-1679a",
+    storageBucket: "",
+    messagingSenderId: "618216615643",
+    appId: "1:618216615643:web:7423fda1b358945a"
+};
+
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database();
+
+
+function removeSchedule(deleteKey) {
+
+    //alert(deleteKey);
+
+    //var db = firebase.database();                  
+    //var ref = db.ref();
+    //var survey=db.ref(path+'/'+path);    //Eg path is company/employee               
+    //ref.child(deleteKey).remove();
+    database.ref("/trainSchedule").child(deleteKey).remove();
+    location.reload(true);
+
+}
+
+
+
+
+
+
 $(document).ready(function () {
     // Initialize Firebase // 
+    /*
     let firebaseConfig = {
         apiKey: "AIzaSyC0duOMYn3EHuQNBy204CxfF7eWE3mVZfA",
         authDomain: "train-scheduler-1679a.firebaseapp.com",
@@ -14,6 +50,7 @@ $(document).ready(function () {
     firebase.initializeApp(firebaseConfig);
 
     let database = firebase.database();
+    */
 
     $("#add-train-btn").on("click", function (event) {
 
@@ -42,7 +79,7 @@ $(document).ready(function () {
 
     database.ref("/trainSchedule").on("child_added", function (snapshot) {
         let trainSchedule = snapshot.val();
-
+        let trainKey = snapshot.key;
         //console.log(trainSchedule);
         //console.log("key " + snapshot.key);
         //console.log("Train " + trainSchedule.firstTrain);
@@ -70,16 +107,19 @@ $(document).ready(function () {
 
         //$("#minAway").trainScheduleHTML = minutesAway;
 
-        let nextTrain = moment().add(minutesAway, "minutes");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+        let nextTrain = moment().add(minutesAway, "minutes").format("hh:mm");
+
+        //console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
         let trainScheduleHTML = `
         <tr>
         <td scope="col">${trainSchedule.trainName}</td>
         <td scope="col">${trainSchedule.destination}</td>
         <td scope="col">${trainSchedule.frequency}</td>
-        <td scope="col">Nest Arrival</td>
+        <td scope="col">${nextTrain}</td>
         <td scope="col">${minutesAway}</td>
+        <td><button id="delete_btn" onclick=removeSchedule('${trainKey}')>Delete</button></td>
+ 
         </tr>
         `;
 
